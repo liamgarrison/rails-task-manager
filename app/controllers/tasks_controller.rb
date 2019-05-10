1a83103059bf8cbdbba032728533faa5483ea6c8
Liam Garrison <liamgarrison@gmail.com>
2019-05-10 13:16:21 +0100

    added create routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,25 @@ class TasksController < ApplicationController
   def edit
     @task = Task.find(params[:id])
   end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(restaurant_params)
+    redirect_to task_path
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(restaurant_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   # resources :tasks
+  # Create
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+
   # Read
   get 'tasks', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # Update
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
 end
